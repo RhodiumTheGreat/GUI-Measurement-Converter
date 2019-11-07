f873f45293af68029939ae88cc0845852955ce83
Rhodium <rhodiumh@gmail.com>
2019-11-07 09:53:01 +0000

    Ahh

---
+++ src/MainWindowController.java
@@ -0,0 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+public class MainWindowController {
+    @FXML
+    private ComboBox conversionTypeSelector;
+
+    @FXML
+    private ComboBox leftConversionSelector;
+
+    @FXML
+    private ComboBox rightConversionSelector;
+}
