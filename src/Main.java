f9c7e72f7057c39c2c6b0b78d2022b14c5572961
Rhodium <rhodiumh@gmail.com>
2019-11-07 15:23:33 +0000

    Initial Conversions File

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private ConversionManager conversionManager;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
         stage.setTitle("Measurement Converter");
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void init() throws Exception {
+        conversionManager = new ConversionManager();
+        conversionManager.initialise();
     }
 
     public static void main(String[] args) {
